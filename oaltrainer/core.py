c004a04aa3f2b10595d4f826eb42ec35f5da461f
vgonisanz <vgonisanz@gmail.com>
2021-07-25 23:08:43 +0200

    core: Added basic elements to the Core

--- oaltrainer/core.py
+++ oaltrainer/core.py
@@ -2,17 +2,44 @@ from openal import *
 
 import time
 
+class FixedPositions:
+    CENTER = [0, 0, 0]
+
+class FixedOrientations:
+    FRONT = [0, 0, 0]
+
+class FixedVelocities:
+    STOPPED = [0, 0, 0]
+
 class Core:
     def __init__(self):
         print('loading_openal')
-        self._source = oalOpen("resources/ak-47_fire-01.wav")
+        self._initialize_listener()
+        self._initialize_source()
 
-    def run(self):
+    def run(self, sound_file):
+        self._source.set_buffer(sound_file)
         self._source.play()
         while self._source.get_state() == AL_PLAYING:
 	        time.sleep(1)
 
-    # Deleting (Calling destructor)
     def __del__(self):
         print('unloading_openal')
         oalQuit()
+    
+    def _initialize_listener(self):
+        self._listener = openal.oalGetListener()
+        self._listener.set_position(FixedPositions.CENTER)
+        self._listener.set_orientation(FixedOrientations.FRONT)
+        self._listener.set_velocity(FixedVelocities.STOPPED)
+        self._listener.set_gain(1.0)
+    
+    def _initialize_source(self):
+        self._source = oalOpen()
+        self._source.set_pitch(1.0)
+        self._source.set_gain(1.0)
+        self._source.set_max_distance(1.0)
+        self._source.set_rolloff_factor(1.0)
+        self._source.set_position(FixedPositions.CENTER)
+        self._source.set_velocity(FixedVelocities.STOPPED)
+        self._source.set_looping(False)
--- oaltrainer/samples/play.py
+++ oaltrainer/samples/play.py
@@ -4,7 +4,7 @@ from oaltrainer.core import Core
 
 def main():
 	core = Core()
-	core.run()
+	core.run("resources/ak-47_fire-01.wav")
 
 if __name__ == "__main__":
 	typer.run(main)
